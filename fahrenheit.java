868dd084340410f9d245de73e3b56b4ab0d7aff4
Praveenk26081 <praveenkumarr.kri@outlook.com>
2024-07-11 12:17:32 +0530

    Add files via upload

---
+++ fahrenheit.java
@@ -0,0 +1,12 @@
+class fahrenheit
+{
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double f,c;
+        System.out.println("Enter the fahrenheit ");
+        f=sc.nextDouble();
+        C = (F - 32) * 5/9;
+        System.out.println("fahrenheit to celsius  = "+c);
+    }
+}
